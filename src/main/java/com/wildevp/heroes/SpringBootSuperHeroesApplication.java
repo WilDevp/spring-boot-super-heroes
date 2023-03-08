6e201141d2ca11525713457a5665767f10142ef3
WilDevp <wilmar.garciava@gmail.com>
2023-03-08 13:49:24 -0500

    feat: :art: First commit
    Config properties and dependencies for use database
    
    01

---
+++ src/main/java/com/wildevp/heroes/SpringBootSuperHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.wildevp.heroes;
+
+import  org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSuperHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSuperHeroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildevp/heroes/SpringBootSuperHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildevp.heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSuperHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
