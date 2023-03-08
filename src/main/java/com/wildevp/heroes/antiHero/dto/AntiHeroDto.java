51b7c4aa05358c6040726b2c036c665451f40523
WilDevp <wilmar.garciava@gmail.com>
2023-03-08 14:12:46 -0500

    feat: :art: Config business model
    Add a dto and entity clases
    
    02

---
+++ src/main/java/com/wildevp/heroes/antiHero/dto/AntiHeroDto.java
@@ -0,0 +1,18 @@
+package com.wildevp.heroes.antiHero.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class AntiHeroDto {
+    private UUID id;
+    @NotNull(message = "First name is required")
+    private String firstName;
+    private String lastName;
+    private String house;
+    private String knownAs;
+}
---
+++ src/main/java/com/wildevp/heroes/antiHero/entity/AntiHeroEntity.java
@@ -0,0 +1,30 @@
+package com.wildevp.heroes.antiHero.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class AntiHeroEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "UUID")
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+    @NotNull(message = "First name is required")
+    private String firstName;
+    private String lastName;
+    private String house;
+    private String knownAs;
+    private String createAt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z").format(new Date());;
+}
